f9eb103d7d07b449ce7c7a320cf4d01bf46ceae9
Jesus AW <jalvarado28@csuchico.edu>
2023-11-02 10:06:48 -0700

    Added Firebase Storage

---
+++ assignmentproject/lib/storage.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import "package:flutter/foundation.dart";
+import 'package:cloud_firestore/cloud_firestore.dart';
+import "package:firebase_core/firebase_core.dart";
+import 'firebase_options.dart';
+
+class HealthApp {
+//  await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+// );
+}
